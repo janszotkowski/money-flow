4ca9d0d2d4afce633a53095762eb8400d98cfe2e
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-03-26 20:18:04 +0100

    refactor: odstranění komponenty Navigation a Outlet z hlavní routy pro zjednodušení struktury

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,10 @@
-import { Layout, Navigation } from '@/components';
+import { Layout } from '@/components';
 import { NotFound } from '@/pages';
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
     component: () => (
-        <Layout>
-            <Navigation />
-            <Outlet />
-        </Layout>
+        <Layout />
     ),
     notFoundComponent: () => <NotFound />,
 });
